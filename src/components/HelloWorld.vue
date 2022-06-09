9f4174eab298af181eff1641e2707f3b8ffaea58
Yang <j.xiang@campus.unimib.it>
2022-06-09 18:00:17 +0200

    added likes functionality

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,26 +11,31 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>
+  <!-- <p>
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
     +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
+  </p> -->
 
-  <p>
+  <!-- <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
       Vite Documentation
     </a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+  </p> -->
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
+  <button type="button" @click="count++">👍</button>
+
+  <p v-if="count == 1">I've got {{ count }} like!</p>
+  <p v-else-if="count > 1">I've got {{ count }} likes!</p>
+  <p v-else>No likes :(</p>
+
+  <!-- <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  </p> -->
 </template>
 
 <style scoped>
